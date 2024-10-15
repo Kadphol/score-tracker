3b436564f9670032bd29ef701641e9cacac3a98f
Mark-Kadphol <kadphol.l@7solutions.co.th>
2024-10-15 13:45:46 +0700

    chore: add prettier and shadcn

---
+++ eslint.config.mjs
@@ -0,0 +1,6 @@
+// @ts-check
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import withNuxt from './.nuxt/eslint.config.mjs'
+
+export default withNuxt(eslintPluginPrettierRecommended)
+// Your custom configs here
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,22 @@
+import { defineNuxtConfig } from 'nuxt/config'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss', 'shadcn-nuxt'],
+  typescript: {
+    typeCheck: true,
+  },
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui',
+  },
 })
